bc9f32b8aeec29de1d0f0742a083746deafb9383
Chinwenm Okorie <okoriechinwe1327@gmail.com>
2025-04-11 11:14:42 +0100

    aos

--- src/types/aos.d.ts
+++ src/types/aos.d.ts
@@ -1,10 +1,15 @@
+// /types/aos.d.ts
+
 declare module 'aos' {
-    const AOS: {
-      init: (options?: any) => void;
-      refresh: () => void;
-      refreshHard: () => void;
-    };
-  
-    export default AOS;
+  interface AosOptions {
+    duration?: number;
+    easing?: string;
+    once?: boolean;
+    mirror?: boolean;
+    anchorPlacement?: string;
   }
-  
+
+  export function init(options?: AosOptions): void;
+  export function refresh(): void;
+  export function refreshHard(): void;
+}
