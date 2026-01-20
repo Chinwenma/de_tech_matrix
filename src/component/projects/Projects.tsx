91a9b72252269c71172826682d7a6bed7f694446
Chinwenm Okorie <okoriechinwe1327@gmail.com>
2026-01-20 17:15:01 +0100

    Refactor Project component for improved structure and add live link functionality

--- src/component/projects/Projects.tsx
+++ src/component/projects/Projects.tsx
@@ -4,42 +4,48 @@ import { projectData } from "../../constants/constants";
 import { ProjectType } from "../../types/types"; // Correct path to your types file
 import Link from "next/link";
 
-
 // Project Component
 const Project = ({ project }: { project: ProjectType }) => {
   return (
     <section id="projects">
-    <div className=" rounded-md shadow-lg overflow-hidden md:h-[74vh] transform transition duration-300" data-aos="flip-left">
-      <Image
-        src={project.image}
-        alt={project.title}
-        width={500}
-        height={300}
-        className="w-full h-60 object-cover object-top hover:scale-105 transition-transform duration-300"
-      />
-      <div className="p-4 ">
-        <h3 className="text-xl font-semibold mb-2">{project.title}</h3>
-        <p className="mb-4 text-justify leading-relaxed tracking-tight">{project.description}</p>
-        <div className="flex px-6 items-center justify-between ">
-          <Link
-            href={project.liveLink as string}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-blue-600 hover:text-blue-800 flex items-center"
-          >
-            <FaExternalLinkAlt className="mr-1" /> Live
-          </Link>
-          <Link
-            href={project.githubLink}
-            target="_blank"
-            rel="noopener noreferrer"
-            className=" hover:text-black flex items-center"
-          >
-            <FaGithub className="mr-1" /> GitHub
-          </Link>
+      <div
+        className=" rounded-md shadow-lg overflow-hidden md:h-[74vh] transform transition duration-300"
+        data-aos="flip-left"
+      >
+        <Image
+          src={project.image}
+          alt={project.title}
+          width={500}
+          height={300}
+          className="w-full h-60 object-cover object-top hover:scale-105 transition-transform duration-300"
+        />
+        <div className="p-4 ">
+          <h3 className="text-xl font-semibold mb-2">{project.title}</h3>
+          <p className="mb-4 text-justify leading-relaxed tracking-tight">
+            {project.description}
+          </p>
+          <div className="flex px-6 items-center justify-between ">
+            {project.liveLink && (
+              <Link
+                href={project.liveLink}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-blue-600 hover:text-blue-800 flex items-center"
+              >
+                <FaExternalLinkAlt className="mr-1" /> Live
+              </Link>
+            )}
+            <Link
+              href={project.githubLink}
+              target="_blank"
+              rel="noopener noreferrer"
+              className=" hover:text-black flex items-center"
+            >
+              <FaGithub className="mr-1" /> GitHub
+            </Link>
+          </div>
         </div>
       </div>
-    </div>
     </section>
   );
 };
@@ -51,7 +57,8 @@ const ProjectSection: React.FC = () => {
       <div className="max-w-6xl mx-auto px-4 text-center">
         <h2 className="text-4xl font-bold mb-4">Projects</h2>
         <p className="mb-12 max-w-2xl mx-auto">
-          A selection of some recent works that showcase my skills in frontend development and UI design.
+          A selection of some recent works that showcase my skills in frontend
+          development and UI design.
         </p>
         <div className="grid gap-8 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
           {projectData.map((project) => (
